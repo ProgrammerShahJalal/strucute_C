50b263af70cc5a58f64b4e3622aad5fccd383cb1
ProgrammerShahJalal <you@example.com>
2022-12-07 18:41:32 +0600

    structure array

---
+++ array-of-structure.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct Student
+{
+    double weight;
+    int roll;
+};
+
+int main()
+{
+    struct Student s[10];
+
+    for(int i=0; i<10; i++){
+        s[i].roll=1+i;
+        s[i].weight=41.75+ (3*i);
+    }
+    for(int i=0; i<10; i++){
+        printf("roll: %d and weight: %.2lf\n", s[i].roll, s[i].weight);
+    }
+    return 0;
+}
